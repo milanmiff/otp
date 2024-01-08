5255018c58476f89691d97f9274618c33b0472e6
Milan Djordjevic <milan.djordjevic@vegait.rs>
2024-01-08 11:16:12 +0100

    Initial Commit

---
+++ OTP/OTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPApp.swift
+//  OTP
+//
+//  Created by Milan Djordjevic on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct OTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
